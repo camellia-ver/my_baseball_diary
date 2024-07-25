70bbf430fb46d47bb0182407eefbd31e314d060b
camellia-ver <jakahi435@gmail.com>
2024-07-25 15:25:21 +0900

    프로젝트 생성

---
+++ my_baseball_diary/src/main/java/com/jyr/my_baseball_diary/MyBaseballDiaryApplication.java
@@ -0,0 +1,13 @@
+package com.jyr.my_baseball_diary;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MyBaseballDiaryApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MyBaseballDiaryApplication.class, args);
+	}
+
+}
---
+++ my_baseball_diary/src/test/java/com/jyr/my_baseball_diary/MyBaseballDiaryApplicationTests.java
@@ -0,0 +1,13 @@
+package com.jyr.my_baseball_diary;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MyBaseballDiaryApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
