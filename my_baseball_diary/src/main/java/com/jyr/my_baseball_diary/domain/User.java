e25188e6dbb342107cc0566e7dab8a6f416f74a0
camellia-ver <jakahi435@gmail.com>
2024-07-26 14:53:12 +0900

    user domain 작성

---
+++ my_baseball_diary/src/main/java/com/jyr/my_baseball_diary/domain/User.java
@@ -0,0 +1,60 @@
+package com.jyr.my_baseball_diary.domain;
+
+import jakarta.persistence.*;
+import lombok.AccessLevel;
+import lombok.Builder;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import org.springframework.security.core.GrantedAuthority;
+import org.springframework.security.core.authority.SimpleGrantedAuthority;
+import org.springframework.security.core.userdetails.UserDetails;
+
+import java.util.Collection;
+import java.util.List;
+
+@Table(name = "members")
+@NoArgsConstructor(access = AccessLevel.PROTECTED)
+@Getter
+@Entity
+public class User implements UserDetails {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name = "id", updatable = false)
+    private Long id;
+
+    @Column(name = "email", nullable = false, unique = true)
+    private String email;
+
+    @Column(name = "password", nullable = false)
+    private String password;
+
+    @Column(name = "userName", nullable = false)
+    private String userName;
+
+    @Column(name = "favoriteTeamId")
+    private Long favoriteTeamId;
+
+    @Builder
+    public User(Long id, String email, String password, String userName, Long favoriteTeamId, String auth) {
+        this.id = id;
+        this.email = email;
+        this.password = password;
+        this.userName = userName;
+        this.favoriteTeamId = favoriteTeamId;
+    }
+
+    @Override
+    public Collection<? extends GrantedAuthority> getAuthorities() {
+        return List.of(new SimpleGrantedAuthority("user"));
+    }
+
+    @Override
+    public String getUsername() {
+        return email;
+    }
+
+    @Override
+    public String getPassword() {
+        return password;
+    }
+}
