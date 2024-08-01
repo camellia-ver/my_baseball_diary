c7dffb400ee583227048529e8248ab49e637d67b
camellia-ver <jakahi435@gmail.com>
2024-08-01 16:44:55 +0900

    logout기능 추가

--- my_baseball_diary/src/main/java/com/jyr/my_baseball_diary/controller/UserApiController.java
+++ my_baseball_diary/src/main/java/com/jyr/my_baseball_diary/controller/UserApiController.java
@@ -2,9 +2,14 @@ package com.jyr.my_baseball_diary.controller;
 
 import com.jyr.my_baseball_diary.dto.AddUserRequest;
 import com.jyr.my_baseball_diary.service.UserService;
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.ResponseEntity;
+import org.springframework.security.core.context.SecurityContextHolder;
+import org.springframework.security.web.authentication.logout.SecurityContextLogoutHandler;
 import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 
@@ -19,10 +24,10 @@ public class UserApiController {
         return "redirect:/login";
     }
 
-    @PostMapping("/validateEmail")
-    public ResponseEntity<?> vailedateEmail(@RequestParam String email) {
-        boolean result = !userService.checkEmail(email);
-
-        return ResponseEntity.ok(result);
+    @GetMapping("/logout")
+    public String logout(HttpServletRequest request, HttpServletResponse response) {
+        new SecurityContextLogoutHandler().logout(request, response,
+                SecurityContextHolder.getContext().getAuthentication());
+        return "redirect:/login";
     }
 }
--- my_baseball_diary/src/main/java/com/jyr/my_baseball_diary/service/UserService.java
+++ my_baseball_diary/src/main/java/com/jyr/my_baseball_diary/service/UserService.java
@@ -9,6 +9,7 @@ import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 
 import java.time.LocalDateTime;
+import java.util.List;
 
 @Service
 @Transactional(readOnly = true)
@@ -19,6 +20,8 @@ public class UserService{
 
     @Transactional
     public Long join(AddUserRequest dto) {
+        validateDuplicateUser(dto);
+
         return userRepository.save(User.builder()
                 .email(dto.getEmail())
                 .password(bCryptPasswordEncoder.encode(dto.getPassword()))
@@ -29,7 +32,10 @@ public class UserService{
                 .build()).getId();
     }
 
-    public Boolean checkEmail(String email) {
-        return userRepository.findByEmail(email).isEmpty();
+    private void validateDuplicateUser(AddUserRequest dto) {
+        List<User> findUsers = userRepository.findByEmail(dto.getEmail()).stream().toList();
+        if (!findUsers.isEmpty()) {
+            throw new IllegalStateException("이미 존재하는 회원입니다.");
+        }
     }
 }
