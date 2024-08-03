74369b6f9a77875e12d4272eeeb704d88e475219
camellia-ver <jakahi435@gmail.com>
2024-08-03 16:08:46 +0900

    글쓰기 ui 작성중

--- my_baseball_diary/src/main/java/com/jyr/my_baseball_diary/api/DiaryApiController.java
+++ my_baseball_diary/src/main/java/com/jyr/my_baseball_diary/api/DiaryApiController.java
@@ -2,9 +2,10 @@ package com.jyr.my_baseball_diary.api;
 
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RestController;
 
 @RequiredArgsConstructor
-@Controller
+@RestController
 public class DiaryApiController {
 
 }
--- my_baseball_diary/src/main/java/com/jyr/my_baseball_diary/controller/DiaryController.java
+++ my_baseball_diary/src/main/java/com/jyr/my_baseball_diary/controller/DiaryController.java
@@ -1,15 +1,33 @@
 package com.jyr.my_baseball_diary.controller;
 
 import lombok.RequiredArgsConstructor;
+import org.springframework.security.core.context.SecurityContextHolder;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
 
 @RequiredArgsConstructor
 @Controller
 public class DiaryController {
     @GetMapping("/main")
     public String mainPage() {
-        return "articlesList";
+        if (SecurityContextHolder.getContext().getAuthentication().getAuthorities()
+                .stream().anyMatch(grantedAuthority -> grantedAuthority.
+                                getAuthority().equals("ROLE_ADMIN"))) {
+            return "admin";
+        } else {
+            return "articlesList";
+        }
+    }
+
+    @GetMapping("/writeForm")
+    public String writePage() {
+        return "write";
+    }
+
+    @PostMapping("/write")
+    public String write() {
+        return "redirect:/articlesList";
     }
 
     @GetMapping("/admin/test")
