04830e3ed2e1c6f36b4b0d7dc0bb6e277b121b0a
camellia-ver <jakahi435@gmail.com>
2024-08-05 16:54:18 +0900

    글쓰기 ui 완료

---
+++ my_baseball_diary/src/main/resources/static/js/write.js
@@ -0,0 +1,20 @@
+document.addEventListener('DOMContentLoaded', function() {
+    var today = new Date();
+    var dd = String(today.getDate()).padStart(2, '0');
+    var mm = String(today.getMonth() + 1).padStart(2, '0');
+    var yyyy = today.getFullYear();
+
+    var todayFormatted = yyyy + '-' + mm + '-' + dd;
+    document.getElementById('date').value = todayFormatted;
+
+    var simplemde = new SimpleMDE({
+        element: document.getElementById("markdown-editor"),
+        spellChecker: false, // 스펠 체크 사용 여부 (옵션)
+        toolbar: [
+          "bold", "italic", "heading", "|",
+          "quote", "code", "unordered-list", "ordered-list", "|",
+          "link", "image", "table", "|",
+          "preview", "side-by-side", "fullscreen"
+        ]
+      });
+});
