9658daf02132f2e07468ce1af953f338274227ca
camellia-ver <jakahi435@gmail.com>
2024-10-18 15:18:53 +0900

    admin page의 팀목록 화면 개발 중

--- my_baseball_diary/src/main/java/com/jyr/my_baseball_diary/controller/BaseballTeamController.java
+++ my_baseball_diary/src/main/java/com/jyr/my_baseball_diary/controller/BaseballTeamController.java
@@ -4,8 +4,8 @@ import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 
 @Controller
-public class adminController {
-    @GetMapping("/teamList")
+public class BaseballTeamController {
+    @GetMapping("/admin/teamList")
     public String teamList() {
         return "teamList";
     }
---
+++ my_baseball_diary/src/main/java/com/jyr/my_baseball_diary/service/BaseballTeamService.java
@@ -0,0 +1,11 @@
+package com.jyr.my_baseball_diary.service;
+
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
+
+@Service
+@Transactional(readOnly = true)
+@RequiredArgsConstructor
+public class BaseballTeamService {
+}
